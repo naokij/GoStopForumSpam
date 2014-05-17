71ab68be3eceae3ff55b3577047a11484fd9996f
David Jiang <smartynaoki@gmail.com>
2014-05-17 11:53:41 +0800

    Add api to submit spam, better error handling for Search

--- stopforumspam/stopforumspam.go
+++ stopforumspam/stopforumspam.go
@@ -25,15 +25,35 @@ type SearchData struct {
 	Username string
 }
 
+type SpamData struct {
+	SearchData
+	Evidence string `json:"evidence"`
+}
+
 type SearchResponseItem struct {
-	Appears    int       `json:"appears"`
-	Frequency  int       `json:"frequency"`
-	LastSeen   time.Time `json:"lastseen"`
-	Confidence float64   `json:"confidence"`
+	Appears    int     `json:"appears"`
+	Frequency  int     `json:"frequency"`
+	LastSeen   Time    `json:"lastseen"`
+	Confidence float64 `json:"confidence"`
+}
+
+type Time time.Time
+
+func (t *Time) UnmarshalJSON(data []byte) error {
+	var err error
+	var tmpTime time.Time
+	trimedData := data[1 : len(data)-1]
+	tmpTime, err = time.Parse(`2006-01-02 15:04:05`, string(trimedData))
+	*t = Time(tmpTime)
+	return err
+}
+
+func (t *Time) ToStdTime() time.Time {
+	stdTime := time.Time(*t)
+	return stdTime
 }
 
 type SearchResponse struct {
-	Success  int                 `json:"success"`
 	Ip       *SearchResponseItem `json:"ip"`
 	Email    *SearchResponseItem `json:"email"`
 	Username *SearchResponseItem `json:"username"`
@@ -78,5 +98,35 @@ func (c *Client) Search(searchData SearchData) (response SearchResponse, err err
 	if err != nil {
 		return response, err
 	}
+	if response.Error != "" {
+		return response, errors.New(response.Error)
+	}
 	return response, nil
+}
+
+func (c *Client) Add(spamData SpamData) (err error) {
+	if spamData.Email == "" || spamData.Ip == "" || spamData.Username == "" || spamData.Evidence == "" {
+		return errors.New("stopforumspam.Add error: spamData not complete")
+	}
+	postValues := url.Values{}
+	postValues.Add("ip_addr", spamData.Ip)
+	postValues.Add("email", spamData.Email)
+	postValues.Add("username", spamData.Username)
+	postValues.Add("evidence", spamData.Evidence)
+	postValues.Add("api_key", c.apiKey)
+	var resp *http.Response
+	resp, err = http.PostForm(ADD_ENDPOINT, postValues)
+	if err != nil {
+		return err
+	}
+	var body []byte
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("stopforumspam.Add error: %d %s", resp.StatusCode, body)
+		return err
+	}
+	return nil
 }
